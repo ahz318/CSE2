b3557242d393068fba0b3b204f2441aa99d60f72
ahz318 <a.zeine.96@hotmail.com>
2015-09-01 05:09:54 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+//Ayman Zeine
+//hw01_WelcomeClass
+//Tuesday 9/1/2015
+
+//Declare class
+public class WelcomeClass
+{
+    //Declare main method
+    public static void main(String[] args)
+    {
+        //Print message
+        System.out.println("  --------------------");
+        System.out.println("  |      WELCOME     |");
+        System.out.println("  --------------------");
+        
+        System.out.println("  ^   ^   ^   ^   ^   ^ ");
+        System.out.println(" / \\ / \\ / \\ / \\ / \\ / \\ ");
+        System.out.println("<-A---H---Z---3---1---8->");
+        System.out.println(" \\ / \\ / \\ / \\ / \\ / \\ / ");
+        System.out.println("  v   v   v   v   v   v  ");
+        
+        System.out.println("This August my wife and I took an eleven-day, 675-mile bike ");
+        System.out.println("tour on our tandem. We circled Lake Ontario.");
+        
+        //Close main method
+    }
+    //Close class
+}
